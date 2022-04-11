832f392dfcdf2b89882fb45384dcafddb9daf081
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-11 14:42:57 +0800

    struct study

---
+++ struct_omitempty/main.go
@@ -0,0 +1,43 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-11 14:34:30
+ * @LastEditTime: 2022-04-11 14:40:09
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /struct_json/Users/bytedance/go/src/github.com/singgel/golang-base/struct_omitempty/main.go
+ */
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	Name     string   `json:"name"`
+	Email    string   `json:"email,omitempty"`
+	Hobby    []string `json:"hobby,omitempty"`
+	*Profile `json:"profile,omitempty"`
+}
+
+type Profile struct {
+	Website string `json:"site"`
+	Slogan  string `json:"slogan"`
+}
+
+func nestedStructDemo() {
+	u1 := User{
+		Name:  "七米",
+		Hobby: []string{"足球", "双色球"},
+	}
+	b, err := json.Marshal(u1)
+	if err != nil {
+		fmt.Printf("json.Marshal failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("str:%s\n", b)
+}
+
+func main() {
+	nestedStructDemo()
+}
---
+++ struct_tag/main.go
@@ -0,0 +1,35 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-11 14:34:40
+ * @LastEditTime: 2022-04-11 14:36:43
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /struct_json/Users/bytedance/go/src/github.com/singgel/golang-base/struct_tag/main.go
+ */
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+//Student 学生
+type Student struct {
+	ID     int    `json:"id"` //通过指定tag实现json序列化该字段时的key
+	Gender string //json序列化是默认使用字段名作为key
+	name   string //私有不能被json包访问
+}
+
+func main() {
+	s1 := Student{
+		ID:     1,
+		Gender: "男",
+		name:   "沙河娜扎",
+	}
+	data, err := json.Marshal(s1)
+	if err != nil {
+		fmt.Println("json marshal failed!")
+		return
+	}
+	fmt.Printf("json str:%s\n", data) //json str:{"id":1,"Gender":"男"}
+}
