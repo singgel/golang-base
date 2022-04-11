851cf74078fdae404e864e2372864fc1cbb929d1
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-11 21:36:45 +0800

    func study

--- func_defer/main.go
+++ func_defer/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-11 15:33:51
- * @LastEditTime: 2022-04-11 15:50:16
+ * @LastEditTime: 2022-04-11 21:31:03
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /struct_json/Users/bytedance/go/src/github.com/singgel/golang-base/func_defer/main.go
