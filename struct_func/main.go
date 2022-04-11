e383446393bb0587033b5ba36109e0f453c97881
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-11 14:25:31 +0800

    generate demo code

---
+++ struct_func/main.go
@@ -0,0 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-11 14:22:27
+ * @LastEditTime: 2022-04-11 14:22:50
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /struct_json/Users/bytedance/go/src/github.com/singgel/golang-base/struct_func/main.go
+ */
+package main
+
+import "fmt"
+
+type student struct {
+	name string
+	age  int
+}
+
+func main() {
+	m := make(map[string]*student)
+	stus := []student{
+		{name: "小王子", age: 18},
+		{name: "娜扎", age: 23},
+		{name: "大王八", age: 9000},
+	}
+
+	for _, stu := range stus {
+		m[stu.name] = &stu
+	}
+	for k, v := range m {
+		fmt.Println(k, "=>", v.name)
+	}
+}
---
+++ struct_json/main.go
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-11 13:49:39
+ * @LastEditTime: 2022-04-11 14:22:38
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /golang-base/structDemo/struct_json.go
+ */
+package main
+
+import "fmt"
+
+type Person struct {
+	name   string
+	age    int8
+	dreams []string
+}
+
+func (p *Person) SetDreams(dreams []string) {
+	p.dreams = dreams
+}
+
+func main() {
+	p1 := Person{name: "小王子", age: 18}
+	data := []string{"吃饭", "睡觉", "打豆豆"}
+	p1.SetDreams(data)
+
+	// 你真的想要修改 p1.dreams 吗？
+	data[1] = "不睡觉"
+	fmt.Println(p1.dreams) // ?
+}
