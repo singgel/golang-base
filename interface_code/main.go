899f7a9489eeec6cf3977dcfac13e984b26d6848
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-13 10:56:03 +0800

    add readme

--- interface_code/main.go
+++ interface_code/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-11 21:55:23
- * @LastEditTime: 2022-04-12 17:41:24
+ * @LastEditTime: 2022-04-13 10:54:01
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /struct_json/Users/bytedance/go/src/github.com/singgel/golang-base/interface_code/main.go
@@ -44,5 +44,3 @@ func main() {
 
 	Checkout(&WeChat{}) // 现在支持使用微信支付
 }
-
-
