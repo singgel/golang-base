0e4e999c6e34af96b4d84fe760ddd97f823f8b58
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-19 16:49:03 +0800

    并发

--- sync_wg/main.go
+++ sync_wg/main.go
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-04-19 16:27:52
- * @LastEditTime: 2022-04-19 16:28:05
- * @LastEditors: your name
+ * @LastEditTime: 2022-04-19 16:48:23
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /golang-base/sync_wg/main.go
  */
@@ -17,28 +17,37 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
-
+	stopCh := make(chan bool)
 	wg.Add(1)
 
 	go func() {
 
 		defer wg.Done()
 
-		watchDog("【监控狗1】")
+		watchDog("【监控狗1】", stopCh)
 
 	}()
+
+	time.Sleep(5 * time.Second)
+
+	stopCh <- true //发停止指令
 
 	wg.Wait()
 
 }
 
-func watchDog(name string) {
+func watchDog(name string, stopCh chan bool) {
 
 	//开启for select循环，一直后台监控
 
 	for {
 
 		select {
+		case <-stopCh:
+
+			fmt.Println(name, "停止指令已收到，马上停止")
+
+			return
 
 		default:
 
