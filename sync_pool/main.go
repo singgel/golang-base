44fa2b1feb817bae03c1a62cdb9d4c9907f9f2d1
hekuangsheng <hekuangsheng@bytedance.com>
2022-04-25 11:46:52 +0800

    有必要补充一个Java的 对照着看

--- sync_pool/main.go
+++ sync_pool/main.go
@@ -1,9 +1,34 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-20 11:49:43
+ * @LastEditTime: 2022-04-25 11:46:21
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /golang-base/sync_atomic/main.go
+有必要补充一个Java的 对照着看
+
+   public class TheadBlockedQ {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadExcutor excutor = new ThreadExcutor(3);
+        for (int i = 0; i < 10; i++) {
+            excutor.exec(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("线程 " + Thread.currentThread().getName() + " 在帮我干活");
+                }
+            });
+        }
+       excutor.shutdown();
+    }
+}
+*/
+
 package main
 
 import (
- "fmt"
- "sync"
- "sync/atomic"
+	"fmt"
+	"sync"
+	"sync/atomic"
 )
 
 // 用来统计实例真正创建的次数
@@ -11,33 +36,33 @@ var numCalcsCreated int32
 
 // 创建实例的函数
 func createBuffer() interface{} {
- // 这里要注意下，非常重要的一点。这里必须使用原子加，不然有并发问题；
- atomic.AddInt32(&numCalcsCreated, 1)
- buffer := make([]byte, 1024)
- return &buffer
+	// 这里要注意下，非常重要的一点。这里必须使用原子加，不然有并发问题；
+	atomic.AddInt32(&numCalcsCreated, 1)
+	buffer := make([]byte, 1024)
+	return &buffer
 }
 
 func main() {
- // 创建实例
- bufferPool := &sync.Pool{
-  New: createBuffer,
- }
+	// 创建实例
+	bufferPool := &sync.Pool{
+		New: createBuffer,
+	}
 
- // 多 goroutine 并发测试
- numWorkers := 1024 * 1024
- var wg sync.WaitGroup
- wg.Add(numWorkers)
+	// 多 goroutine 并发测试
+	numWorkers := 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
 
- for i := 0; i < numWorkers; i++ {
-  go func() {
-   defer wg.Done()
-   // 申请一个 buffer 实例
-   buffer := bufferPool.Get()
-   _ = buffer.(*[]byte)
-   // 释放一个 buffer 实例
-   defer bufferPool.Put(buffer)
-  }()
- }
- wg.Wait()
- fmt.Printf("%d buffer objects were created.\n", numCalcsCreated)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			// 申请一个 buffer 实例
+			buffer := bufferPool.Get()
+			_ = buffer.(*[]byte)
+			// 释放一个 buffer 实例
+			defer bufferPool.Put(buffer)
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("%d buffer objects were created.\n", numCalcsCreated)
 }
